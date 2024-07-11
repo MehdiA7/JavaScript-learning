a03ec8732eee583bf18e9416f12a49a4e5a51601
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2024-07-11 14:13:01 +0200

    Create My_age_in_dog_years.js

---
+++ My_age_in_dog_years.js
@@ -0,0 +1,14 @@
+// Your name
+const myName = "Mehdi";
+// Your age
+const myAge = 23;
+// Declare variable
+let earlyYears = 2;
+// Multiply earlyYears
+earlyYears = earlyYears * 10.5;
+// Declare a variable and calculate
+let laterYears = (myAge - 2) * 4;
+// Calculate the result of the question
+let myAgeInDogYears = earlyYears + laterYears;
+// Print the final result with text
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
