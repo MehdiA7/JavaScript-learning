dc6946324311d647d0ac37cb7d16c28afa9f5b48
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2024-07-11 21:42:39 +0200

    Finish Magic_eight_ball.js

--- Magic_eight_ball.js
+++ Magic_eight_ball.js
@@ -1,4 +1,4 @@
-let userName = "";
+let userName = "Mehdi";
 
 if (userName) {
     console.log(`Hello, ${userName}!`);
@@ -6,19 +6,48 @@ if (userName) {
     console.log("Hello !");
 }
 
-let userQuestion = "";
+let userQuestion = "I'm good ?";
+let eightBall = "";
+let randomNumber = Math.floor(Math.random() * 8);
 
 if (userQuestion) {
     if (userName) {
-    console.log(`${userName} ask : ${userQuestion}`);
+        console.log(`${userName} ask : ${userQuestion}`);
     } else {
-    console.log(`You ask : ${userQuestion}`);
+        console.log(`You ask : ${userQuestion}`);
+
+    } switch (randomNumber) {
+        case 0:
+            eightBall = 'It is certain';
+            break;
+        case 1:
+            eightBall = 'It is decidedly so';
+            break;
+        case 2:
+            eightBall = 'Reply hazy try again';
+            break;
+        case 3:
+            eightBall = 'Cannot predict now';
+            break;
+        case 4:
+            eightBall = 'Do not count on it';
+            break;
+        case 5:
+            eightBall = 'My sources say no';
+            break;
+        case 6:
+            eightBall = 'Outlook not so good';
+            break;
+        case 7:
+            eightBall = 'Signs point to yes';
+            break;
+        default:
+            eightBall = 'Unexpected value';
+            break;
     }
 } else {
     console.log("You had no questions");
 }
 
-let randomNumber = Math.floor(Math.random * 8);
 
-let eightBall = "";
-
+console.log(eightBall)
